811e9a8e57ed8acdfd0be08a7a4293abc9cae71a
Brooke Claroni <brookeclaroni@gwu.edu>
2023-11-25 20:12:52 -0500

    skills and their pics can be added to list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SkillInput from './SkillInput';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 function App() {
+  const [textList, setTextList] = useState([]);
+  const [selectedSkillImage, setSelectedSkillImage] = useState(null);
+  const [skillImages, setSkillImages] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState('');
+
+  const eventOptions = ['vt', 'ub', 'bb', 'fx'];
+
+  function addText(skill, image) {
+    setTextList([...textList, { skill, image }]);
+    setSkillImages([...skillImages, image]);
+  }
+
+  const handleEventChange = (event) => {
+    setSelectedEvent(event.target.value);
+
+    // Reset the skill list when the event changes
+    setTextList([]);
+    setSkillImages([]);
+    setSelectedSkillImage(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Paper elevation={3} style={{ padding: 20, marginTop: 40 }}>
+        <FormControl style={{ marginBottom: 20 }}>
+          <Typography variant="h5" component="div" style={{ marginBottom: 10 }}>
+            Select an event:
+          </Typography>
+          <Select value={selectedEvent} onChange={handleEventChange}>
+            {eventOptions.map((event, index) => (
+              <MenuItem key={index} value={event}>
+                {event}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        {selectedEvent && (
+          <>
+            <Typography variant="h5" component="div" style={{ marginBottom: 20 }}>
+              Select {selectedEvent ? `${selectedEvent} skills` : 'skills'}
+            </Typography>
+
+            <SkillInput
+              addSkill={(skill, image) => addText(skill, image)}
+              setSelectedSkillImage={setSelectedSkillImage}
+              skillImages={skillImages}
+              setSkillImages={setSkillImages}
+              selectedEvent={selectedEvent}
+            />
+          </>
+        )}
+
+        <List>
+          {textList.map((item, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={item.skill} />
+              {item.image && (
+                <Card style={{ width: '150px', marginLeft: '10px' }}>
+                  <CardMedia
+                    component="img"
+                    alt="Skill Image"
+                    height="50"
+                    image={item.image}
+                    style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+                  />
+                </Card>
+              )}
+            </ListItem>
+          ))}
+        </List>
+      </Paper>
+    </Container>
   );
 }
 
---
+++ src/SkillInput.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+import skillsData from './data/skills.json';
+
+function SkillInput({ addSkill, setSelectedSkillImage, skillImages, setSkillImages, selectedEvent }) {
+  const [inputValue, setInputValue] = useState('');
+  const [selectedSkill, setSelectedSkill] = useState(null);
+  const [filteredSkills, setFilteredSkills] = useState([]);
+
+  useEffect(() => {
+      console.log('Selected Event:', selectedEvent);
+
+    // Check if selectedEvent is defined before filtering skills
+    if (selectedEvent) {
+      const skillsForEvent = skillsData.filter((skill) => skill.event === selectedEvent);
+      setFilteredSkills(skillsForEvent);
+    }
+  }, [selectedEvent]);
+
+  function handleSkillSelect(value) {
+    const selectedSkillData = filteredSkills.find((skill) => skill.description === value);
+    if (selectedSkillData) {
+      addSkill(selectedSkillData.description, "images/"+selectedSkillData.img_file);
+      setSelectedSkill(selectedSkillData);
+      setInputValue('');
+      setSelectedSkillImage(selectedSkillData.img_file);
+      setSkillImages([...skillImages, "images/"+selectedSkillData.img_file]);
+    }
+  }
+
+  return (
+    <div style={{ marginTop: 20, marginBottom: 20 }}>
+      <Autocomplete
+        freeSolo
+        options={filteredSkills.map((skill) => skill.description)}
+        value={inputValue}
+        inputValue={inputValue}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Start typing skill..."
+            variant="outlined"
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        )}
+        onInputChange={(e, value) => setInputValue(value)}
+        onChange={(e, value) => handleSkillSelect(value)}
+      />
+    </div>
+  );
+}
+
+export default SkillInput;
