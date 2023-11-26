e095ff7ad118f979438d29a09ccaa747a60b8bbb
Brooke Claroni <brookeclaroni@gwu.edu>
2023-11-25 20:26:44 -0500

    add connection menu and use event words instead of code

--- src/App.js
+++ src/App.js
@@ -12,16 +12,23 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 
+// Mapping function for event codes to formal text
+const mapEventCodeToText = {
+  ub: 'Uneven Bars',
+  bb: 'Balance Beam',
+  fx: 'Floor Exercise',
+};
+
 function App() {
   const [textList, setTextList] = useState([]);
   const [selectedSkillImage, setSelectedSkillImage] = useState(null);
   const [skillImages, setSkillImages] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState('');
 
-  const eventOptions = ['vt', 'ub', 'bb', 'fx'];
+  const eventOptions = ['ub', 'bb', 'fx'];
 
   function addText(skill, image) {
-    setTextList([...textList, { skill, image }]);
+    setTextList([...textList, { skill, image, connection: 'disconnected' }]);
     setSkillImages([...skillImages, image]);
   }
 
@@ -34,6 +41,12 @@ function App() {
     setSelectedSkillImage(null);
   };
 
+  const handleConnectionChange = (index, connection) => {
+    const updatedTextList = [...textList];
+    updatedTextList[index].connection = connection;
+    setTextList(updatedTextList);
+  };
+
   return (
     <Container maxWidth="sm">
       <Paper elevation={3} style={{ padding: 20, marginTop: 40 }}>
@@ -44,7 +57,7 @@ function App() {
           <Select value={selectedEvent} onChange={handleEventChange}>
             {eventOptions.map((event, index) => (
               <MenuItem key={index} value={event}>
-                {event}
+                {mapEventCodeToText[event]}
               </MenuItem>
             ))}
           </Select>
@@ -53,7 +66,7 @@ function App() {
         {selectedEvent && (
           <>
             <Typography variant="h5" component="div" style={{ marginBottom: 20 }}>
-              Select {selectedEvent ? `${selectedEvent} skills` : 'skills'}
+              Select {selectedEvent ? `${mapEventCodeToText[selectedEvent]} skills` : 'skills'}
             </Typography>
 
             <SkillInput
@@ -68,20 +81,35 @@ function App() {
 
         <List>
           {textList.map((item, index) => (
-            <ListItem key={index}>
-              <ListItemText primary={item.skill} />
-              {item.image && (
-                <Card style={{ width: '150px', marginLeft: '10px' }}>
-                  <CardMedia
-                    component="img"
-                    alt="Skill Image"
-                    height="50"
-                    image={item.image}
-                    style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-                  />
-                </Card>
+            <React.Fragment key={index}>
+              <ListItem>
+                <ListItemText primary={item.skill} />
+                {item.image && (
+                  <Card style={{ width: '150px', marginLeft: '10px' }}>
+                    <CardMedia
+                      component="img"
+                      alt="Skill Image"
+                      height="50"
+                      image={item.image}
+                      style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+                    />
+                  </Card>
+                )}
+              </ListItem>
+              {index < textList.length - 1 && ( // Only show connection dropdown between skills
+                <ListItem key={`connection-${index}`}>
+                  <FormControl style={{ marginLeft: '10px' }}>
+                    <Select
+                      value={item.connection}
+                      onChange={(e) => handleConnectionChange(index, e.target.value)}
+                    >
+                      <MenuItem value="connected">Connected</MenuItem>
+                      <MenuItem value="disconnected">Not Connected</MenuItem>
+                    </Select>
+                  </FormControl>
+                </ListItem>
               )}
-            </ListItem>
+            </React.Fragment>
           ))}
         </List>
       </Paper>
